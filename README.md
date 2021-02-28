8e8bb4f42c50c8cff24db5b87f9af5ce1134a17d
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2021-02-27 23:56:59 -0500

    added walkthrough video link to readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 The Employee Tracker is a back-end Node.js application designed for business owners built with MySQL, Inquirer, and console.table package. This application allows business owners to view and manage the departments, roles, and employees in their company in order to organize and plan their business.
 
-A demo of how to use this application and view the functionality can be found here:
+A demo of how to use this application and view functionality can be found here: https://drive.google.com/file/d/103XJUIi6rvGCQBXLkA9UjBr43bD_VtGb/view
--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,6 @@ CREATE TABLE roles(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT UNSIGNED NOT NULL,
-    -- CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 
@@ -27,8 +26,5 @@ CREATE TABLE employees(
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     manager_id INT UNSIGNED,
-    -- CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-    -- manager_id INT,
-    -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
